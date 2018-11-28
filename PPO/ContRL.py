8f30733f5fadc1c772bd229de1375b34937b686f
Nick <nickwalton00@gmail.com>
2018-11-28 13:33:32 -0700

    update

--- PPO/ContRL.py
+++ PPO/ContRL.py
@@ -8,14 +8,19 @@ def RunPendulum():
     model, avg_rewards = ppo_learn(env, model, episode_length=1000, render=True, render_wait=0.2)
 
 
-def RunBipedal(load_model=False):
+def RunBipedal(load_model=False, model_path = './biped_model'):
     env = gym.make('BipedalWalker-v2')
 
     if load_model:
-    model = ActorCritic(env.observation_space.shape[0], env.action_space.shape[0], hidden_size=32)
+        model = torch.load(model_path)
+        model.eval()
+    else:
+        model = ActorCritic(env.observation_space.shape[0], env.action_space.shape[0], hidden_size=32)
 
-    model, avg_rewards = ppo_learn(env, model, episode_length=1000, render=True)
+    model, avg_rewards = ppo_learn(env, model, episode_length=2000, render=True, epochs=200)
+
+    torch.save(model, model_path)
 
 
 if __name__=='__main__':
-    RunBipedal()
+    RunBipedal(load_model=True)
